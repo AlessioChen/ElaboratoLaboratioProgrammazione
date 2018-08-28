74d3d0b05671a84254a4de2b6313c91180157a73
alessio <alessiochen>
2018-08-28 11:41:35 +0200

    aggiornata interfaccia grafica

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
 
 #include <QApplication>
 #include <QTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTimeEdit>
 #include "mainwindow.h"
+#include "Counter.h"
 
 int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
 
-    MainWindow window;
-    window.show();
+    MainWindow *window = new MainWindow;
+    window->setGeometry(500, 200, 650, 450);
+
+    Counter contatore;
+
+
+    //bottone di start
+    QPushButton start("start", window);
+    start.setGeometry(20, 150, 120, 50);
+    QFont font = start.font();
+    font.setPointSize(15);
+
+    //bottone di stop
+    QPushButton stop("stop", window);
+    stop.setGeometry(320, 150, 120, 50);
+    font = stop.font();
+    font.setPointSize(15);
+
+
+    //editor per il tempo
+    QTimeEdit editor(window);
+    editor.setGeometry(140, 150, 180, 50);
+    editor.setAlignment(Qt::AlignCenter);
+    font = editor.font();
+    font.setPointSize(15);
+
+    window->show();
 
     return app.exec();
 
